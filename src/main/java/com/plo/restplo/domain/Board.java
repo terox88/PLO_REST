eddfc647299677018aa4a802ca52fcdf39aa7897
Michal <terox@wp.pl>
2024-08-20 21:55:45 +0200

    Added Entity for Game

--- src/main/java/com/plo/restplo/domain/Board.java
+++ src/main/java/com/plo/restplo/domain/Board.java
@@ -1,23 +1,38 @@
 package com.plo.restplo.domain;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
-@RequiredArgsConstructor
+
 @Getter
+@NoArgsConstructor
 @EqualsAndHashCode
+@Setter
+@Entity
 public class Board {
-    private final Game game;
-    private final ViperGorge viperGorge;
-    private final List<ActionField> actionFields;
-    private final List<Land> lands;
-    private final List<HeroCard> heroCards;
-    private final InitiativeTrack initiativeTrack;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", unique = true)
+    private long id;
+    private  ViperGorge viperGorge;
+    private  List<ActionField> actionFields;
+    private  List<Land> lands;
+    private  List<HeroCard> heroCards;
+    private  InitiativeTrack initiativeTrack;
     private int deadSnowTrack;
-    private final int deadSnowLimit;
-    private final List<InfluenceMarker> iceGarden;
+    private  int deadSnowLimit;
+    private  List<InfluenceMarker> iceGarden;
 
-
+    public Board(ViperGorge viperGorge, List<ActionField> actionFields, List<Land> lands, List<HeroCard> heroCards, InitiativeTrack initiativeTrack, int deadSnowTrack, int deadSnowLimit) {
+        this.viperGorge = viperGorge;
+        this.actionFields = actionFields;
+        this.lands = lands;
+        this.heroCards = heroCards;
+        this.initiativeTrack = initiativeTrack;
+        this.deadSnowTrack = deadSnowTrack;
+        this.deadSnowLimit = deadSnowLimit;
+        this.iceGarden = new ArrayList<>();
+    }
 }
--- src/main/java/com/plo/restplo/domain/Game.java
+++ src/main/java/com/plo/restplo/domain/Game.java
@@ -1,20 +1,32 @@
 package com.plo.restplo.domain;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
 
 import java.util.List;
-@RequiredArgsConstructor
+@Entity(name = "GAMES")
 @Getter
 @Setter
 @EqualsAndHashCode
 public class Game {
-    private final Board board;
-    private final List<Player> players;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", unique = true)
+    private long id;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "BOARD_ID")
+    private  Board board;
+    private List<Player> players;
     private Player activePlayer;
     private boolean isEnded;
     private Player winner;
 
+    public Game() {
+    }
+
+    public Game(Board board, List<Player> players) {
+        this.board = board;
+        this.players = players;
+    }
 }
